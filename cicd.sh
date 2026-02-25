0d95e9224536130aabb963b74d3633e2a9044f85
Felipe O Mello <felipeodorizi@gmail.com>
2026-02-25 11:19:58 -0300

    Update cicd shell

--- cicd.sh
+++ cicd.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
 
-echo "Chamar rotinas de CICD"
+echo "################################################################"
+echo "#######   START of the bash script   (git_pull_ecflow).  #######"
+echo "################################################################"
+
+cd /home/linux/SMNA_CICD || { echo "❌ Diretório não encontrado"; exit 1; }
+
+echo "Executando git pull..."
+if ! git pull origin develop; then
+  echo "❌ Erro ao executar git pull"
+  conda run -n telegram python /home/linux/ecFlow/python/webhook/send_telegram_message.py "❌ Erro ao executar git pull"
+  exit 1
+fi
+echo "✅ git pull concluído com sucesso"
+conda run -n telegram python /home/linux/ecFlow/python/webhook/send_telegram_message.py "✅ git pull concluído com sucesso"
+
+# --- Valida suite ---
+echo "Validando suite..."
+if ! conda run -n ecflow_env python /home/linux/ecFlow/python/webhook/validatesuite.py SMNA_PRE_OPER.def; then
+  echo "❌ Falha na validação da suite"
+  conda run -n telegram python /home/linux/ecFlow/python/webhook/send_telegram_message.py "❌ Falha na validação da suite"
+  exit 1
+fi
+echo "✅ Suite validada com sucesso"
+conda run -n telegram python /home/linux/ecFlow/python/webhook/send_telegram_message.py "✅ Suite validada com sucesso"
+
+
+# --- Executa ecflow_client ---
+echo "Executando ecflow_client..."
+export ECF_HOST=linux-Lenovo-ideapad-330-15IKB
+export ECF_PORT=5500
+
+if ! ecflow_client --replace=/SMNA_PRE_OPER SMNA_PRE_OPER.def; then
+  echo "❌ Erro ao executar ecflow_client"
+  conda run -n telegram python /home/linux/ecFlow/python/webhook/send_telegram_message.py "❌ Erro ao executar ecflow_client"
+  exit 1
+fi
+echo "✅ ecflow_client executado com sucesso"
+conda run -n telegram python /home/linux/ecFlow/python/webhook/send_telegram_message.py "✅ ecflow_client executado com sucesso"
+
+
+echo "################################################################"
+echo "#######   END of the bash script   (git_pull_ecflow).   #######"
+echo "################################################################"
